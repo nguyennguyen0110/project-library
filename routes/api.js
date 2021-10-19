cc91ee3353b9f2534efa0ed1b0f207f48987c7bf
nguyennguyen0110 <85159841+nguyennguyen0110@users.noreply.github.com>
2021-10-19 15:21:02 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,41 +7,103 @@
 */
 
 'use strict';
+//Import Mongoose and connect to MongoDB with URI store in secret
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Create book Schema and Model
+const bookSchema = new mongoose.Schema({
+  title: String,
+  commentcount: Number,
+  comments: [String]
+});
+const Book = new mongoose.model('Book', bookSchema);
 
 module.exports = function (app) {
 
   app.route('/api/books')
     .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      Book.find({}).select('-comments').exec((err, books) => {
+        if (err) return console.log(err);
+        res.json(books);
+      });
     })
     
     .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+      if (!req.body.title) {
+        res.json('missing required field title');
+      }
+      else {
+        Book.findOne({title: req.body.title}, (err, book) => {
+          if (err) return console.log(err);
+          if (book == null) {
+            let newBook = new Book({title: req.body.title, commentcount: 0});
+            newBook.save((err, book) => {
+              if (err) return console.log(err);
+              res.json({title: book.title, _id: book._id});
+            });
+          }
+          else {
+            res.json({'Error': 'This book is already in the library'});
+          }
+        });
+      }
     })
     
     .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+      Book.deleteMany({}, err => {
+        if (err) return console.log(err);
+        res.json('complete delete successful');
+      });
     });
 
 
 
   app.route('/api/books/:id')
     .get(function (req, res){
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      Book.findById(req.params.id, '-commentcount', (err, book) => {
+        if (err) return console.log(err);
+        if (book == null) {
+          res.json('no book exists');
+        }
+        else {
+          res.json(book);
+        }
+      });
     })
     
     .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
-      //json res format same as .get
+      if (!req.body.comment) {
+        res.json('missing required field comment');
+      }
+      else {
+        Book.findByIdAndUpdate(
+          req.params.id,
+          {$push: {comments: req.body.comment}, $inc: {commentcount: 1}},
+          {select: '-commentcount', new: true},
+          (err, book) => {
+            if (err) return console.log(err);
+            if (book == null) {
+              res.json('no book exists');
+            }
+            else {
+              res.json(book);
+            }
+          }
+        );
+      }
     })
     
     .delete(function(req, res){
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+      Book.findByIdAndRemove(req.params.id, (err, book) => {
+        if (err) return console.log(err);
+        if (book == null) {
+          res.json('no book exists');
+        }
+        else {
+          res.json('delete successful');
+        }
+      })
     });
   
 };
